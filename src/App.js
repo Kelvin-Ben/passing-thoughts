ea380ebc1bc11c9a8ee1dd9af5410c251845087a
Kelvin-Ben <kevoben14@gmail.com>
2024-04-12 23:44:18 +0300

    Create a top level component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import './App.css';
+import React, { useState } from "react";
+
+import AddThoughtForm from "./AddThoughtForm";
+import "./App.css";
+import Thought from "./Thought";
 
 function App() {
-return <h1>This is my first line</h1>
+  const [thoughts, setThoughts] = useState([]);
+  const addThought = (thought) => setThoughts((prev) => [thought, ...prev]);
+  return (
+    <div className="App">
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
+      <AddThoughtForm addThought={addThought} />
+      <main>
+        <ul className="thoughts">
+          {thoughts.map((thought) => (
+            <Thought key={thought.id} thought={thought} />
+          ))}
+        </ul>
+      </main>
+    </div>
+  );
 }
 
 export default App;
