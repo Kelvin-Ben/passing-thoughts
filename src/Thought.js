1491276a660b138a73020378b3f7f722a61ce837
Kelvin-Ben <kevoben14@gmail.com>
2024-04-13 11:20:29 +0300

    Create a function to delete thoughts automatically

--- src/Thought.js
+++ src/Thought.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Thought = (props) => {
   const { thought, removeThought } = props;
   function handleClick() {
     removeThought(thought.id);
   }
+  useEffect(() => {
+    const remainingTime = thought.expiresAt - Date.now();
+    const timeout = setTimeout(() => {
+      removeThought(thought.id);
+    }, remainingTime);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [thought]);
   return (
     <li className="Thought">
       <button
